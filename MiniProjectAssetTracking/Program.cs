69538dde8c1fb063f84e9f49dda3d01e22ff2d0f
Karamellen123 <ellen.s.davidsson@gmail.com>
2024-02-22 16:06:47 +0100

    Added the phone class

--- MiniProjectAssetTracking/Program.cs
+++ MiniProjectAssetTracking/Program.cs
@@ -32,39 +32,39 @@ MyComputer.Add(comp2);
 MyComputer.Add(comp3);
 MyComputer.Add(comp4);
 
+
+Phone phone1 = new Phone("Huawei", "Huawei p9 lite", "US", date2, 300);
+Phone phone2 = new Phone("Apple", "Iphone 9", "US", date3, 1000);
+Phone phone3 = new Phone("Samsung", "Samsung galaxy 4", "US", date1, 200);
+Phone phone4 = new Phone("Google", "Google Pixle pro 8", "US", date3, 150);
+Phone phone5 = new Phone("Apple", "Iphone 2", "US", date2, 500);
+MyPhone.Add(phone1);
+MyPhone.Add(phone2);
+MyPhone.Add(phone3);
+MyPhone.Add(phone4);
+MyPhone.Add(phone5);
+
 DisplayLists();
 
 
 void DisplayLists()
 {
-    //Sorter by 
-  //  MyComputer = MyComputer.OrderBy(c => c.PurchaseDate).ToList();
-
-    DateTime currentdate = DateTime.Now;    // Get the current date,  want to modifiy this one
-    DateTime expirationDate = currentdate.AddYears(3);
+    //Sorter by purchase date
+    MyComputer = MyComputer.OrderBy(c => c.PurchaseDate).ToList();
 
     for (int i = 0; i < MyComputer.Count(); i++)
     {
-        // If purchase date + 3 år är mindre än dagens datum blir det rött
         DateTime temp = MyComputer[i].PurchaseDate.AddYears(3);
-       // temp = temp.AddMonths(-3);
         TimeSpan difference = temp - DateTime.Today;
-
-
-
 
         DateTime toOld = MyComputer[i].PurchaseDate.AddYears(3);
         
-
-
         if (toOld <= DateTime.Now || difference.TotalDays <= 0)
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("\nBrand:" + MyComputer[i].Brand + " \nModel: " + MyComputer[i].ModelName + " \nPurchase date:" + MyComputer[i].PurchaseDate);
             Console.WriteLine("Days left: " + Math.Floor(difference.TotalDays));
         }
-        //else if(temp < DateTime.Now )
-        //else if (temp <= DateTime.Now)
         else if (difference.TotalDays >= 0 && difference.TotalDays <= 90)
         {
             
@@ -79,10 +79,37 @@ void DisplayLists()
         }
         Console.ResetColor();
     }
+    Console.WriteLine("_________________________________________________________________");
+
+    MyPhone = MyPhone.OrderBy(p => p.PurchaseDate).ToList();
+
+    for (int i = 0; i < MyPhone.Count(); i++)
+    {
+        DateTime temp = MyPhone[i].PurchaseDate.AddYears(3);
+        TimeSpan difference = temp - DateTime.Today;
+        DateTime toOld = MyPhone[i].PurchaseDate.AddYears(3);
+
+        if (toOld <= DateTime.Now || difference.TotalDays <= 0)
+            textPhone(i, difference, 1);
+        else if (difference.TotalDays >= 0 && difference.TotalDays <= 90)
+            textPhone(i, difference, 2);
+        else
+            textPhone(i, difference, 3);
+        Console.ResetColor();
+    }
     Console.ReadKey();
 }
-
-
+void textPhone(int index, TimeSpan difference, int state)
+{
+    if(state == 1)
+        Console.ForegroundColor = ConsoleColor.Red;
+    if(state == 2)
+        Console.ForegroundColor = ConsoleColor.Yellow;
+    else
+        Console.ResetColor();
+    Console.WriteLine("\nBrand:" + MyPhone[index].Brand + " \nModel: " + MyPhone[index].ModelName + " \nPurchase date:" + MyPhone[index].PurchaseDate);
+    Console.WriteLine("Days left: " + Math.Floor(difference.TotalDays));
+}
 
 void Lifespan()
 {
@@ -92,8 +119,6 @@ void Lifespan()
     TimeSpan timeRemaining = expirationDate - currentdate;
     Console.WriteLine("Time remaining: " + timeRemaining + " \nCurrent date: " + currentdate.ToString() + " \nOld date:" + expirationDate);
 }
-
-
 
 
 class Computer
@@ -120,14 +145,14 @@ class Computer
 
 class Phone
 {
-    public Phone(string phoneType, DateTime purchaseDate, int price, string modelName)
+    public Phone(string brand, string modelName, string office, DateTime purchaseDate, int price )
     {
-        PhoneType = phoneType;
+        Brand = brand;
         PurchaseDate = purchaseDate;
         Price = price;
         ModelName = modelName;
     }
-    public string PhoneType { get; set; }
+    public string Brand { get; set; }
     public DateTime PurchaseDate { get; set; }
     public int Price { get; set;}
     public string ModelName { get; set;}
